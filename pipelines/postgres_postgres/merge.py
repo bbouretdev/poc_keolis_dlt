cfa9e5af5092be00fb4219d4d4f0068e65ed39cf
binachier <binachier@local>
2026-07-07 10:27:15 +0200

    added merge pipeline

--- pipelines/postgres_postgres/merge.py
+++ pipelines/postgres_postgres/merge.py
@@ -1,14 +1,21 @@
 import dlt
 from dlt.sources.sql_database import sql_table
 
+import os
+import json
+
+primary_key = None
+if "DLT_PRIMARY_KEY" in os.environ:
+    primary_key = json.loads(os.environ["DLT_PRIMARY_KEY"])
+
 source = sql_table(
     table="orders",
     schema="dlt",
     backend="connectorx",
     chunk_size=50000,
-).apply_hints(
-    primary_key="order_number"
 )
+if primary_key:
+    source = source.apply_hints(primary_key=primary_key)
 
 pipeline = dlt.pipeline(
     pipeline_name="postgres_postgres_merge",
