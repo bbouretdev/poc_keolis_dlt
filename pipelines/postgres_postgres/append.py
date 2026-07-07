ec6f2b8207960f8292a1d1632c793a698d706a33
binachier <binachier@local>
2026-07-07 09:31:44 +0200

    added append pipeline

---
+++ pipelines/postgres_postgres/append.py
@@ -0,0 +1,24 @@
+import dlt
+from dlt.sources.sql_database import sql_table
+
+source = sql_table(
+    table="orders",
+    schema="dlt",
+    backend="connectorx",
+    chunk_size=50000,
+)
+
+pipeline = dlt.pipeline(
+    pipeline_name="postgres_postgres_append",
+    destination="postgres_dest",
+    dataset_name="dlt"
+)
+
+load_info = pipeline.run(
+    source,
+    table_name="orders",
+    write_disposition="append"
+)
+
+print(pipeline.last_trace)
+print(load_info)
