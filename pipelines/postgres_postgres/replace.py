a68acd8e3234a9953cec239172193aec97dcec21
binachier <binachier@local>
2026-07-07 11:46:40 +0200

    added merge pipeline

--- pipelines/postgres_postgres/replace.py
+++ pipelines/postgres_postgres/replace.py
@@ -4,27 +4,30 @@ from dlt.sources.sql_database import sql_table
 import os
 import json
 
-schema = os.environ["DLT_SOURCE_SCHEMA"]
-table = os.environ["DLT_SOURCE_TABLE"]
+pipeline_id = os.environ["DLT_PIPELINE_ID"]
+source_schema = os.environ["DLT_SOURCE_SCHEMA"]
+source_table = os.environ["DLT_SOURCE_TABLE"]
+target_schema = os.environ["DLT_TARGET_SCHEMA"]
+target_table = os.environ["DLT_TARGET_TABLE"]
 backend = os.environ["DLT_BACKEND"]
 chunk_size = int(os.environ["DLT_CHUNK_SIZE"])
 
 source = sql_table(
-    table=table,
-    schema=schema,
+    table=source_table,
+    schema=source_schema,
     backend=backend,
     chunk_size=chunk_size,
 )
 
 pipeline = dlt.pipeline(
-    pipeline_name="postgres_postgres_replace",
+    pipeline_name=pipeline_id,
     destination="postgres_dest",
-    dataset_name="dlt"
+    dataset_name=target_schema
 )
 
 load_info = pipeline.run(
     source,
-    table_name="orders",
+    table_name=target_table,
     write_disposition="replace"
 )
 
