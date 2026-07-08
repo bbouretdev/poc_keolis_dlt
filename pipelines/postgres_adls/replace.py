b4c71df7e7baf0e5e333fb3216354e0907541575
binachier <binachier@local>
2026-07-08 15:48:14 +0200

    test

--- pipelines/postgres_adls/replace.py
+++ pipelines/postgres_adls/replace.py
@@ -19,28 +19,12 @@ source = sql_table(
     chunk_size=chunk_size,
 )
 
-# destination = filesystem(
-#     bucket_url="az://keolis",
-#     credentials={
-#         "azure_storage_account_name": "devstoreaccount1",
-#         "azure_storage_account_key": "Eby8vdM02xNOcqFeqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==",
-#         "azure_account_host": "azurite:10000/devstoreaccount1",
-#     },
-# )
-
 pipeline = dlt.pipeline(
-    pipeline_name="pif",
+    pipeline_name=pipeline_id,
     destination="filesystem",
-    dataset_name="paf",
+    dataset_name=target_path,
 )
 
-# pipeline = dlt.pipeline(
-#     pipeline_name=pipeline_id,
-#     destination=destination,
-#     dataset_name=target_path,
-# )
-
-# print(destination.configuration)
 load_info = pipeline.run(
     source,
     table_name=target_filename,
